f0aebbc728573c5dbbb5537d517e07f24670641e
panvicka <panvicka@gmail.com>
2021-12-02 23:12:10 +0100

    added global styles

---
+++ frontend/src/index.styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const FlexRowCenter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FlexCol = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FlexColCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+
