057568d7a75eb6262e4c9bc900df78e91443f2e2
panvicka <panvicka@gmail.com>
2021-12-02 21:30:54 +0100

    starting with navigation bar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import './NavBar.scss'
+import "./NavBar.scss";
+import { images } from "../../constants";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav>
+      <div>
+        <img src={images.snecek_logo} alt="snail logo" className="rounded"></img>
+      </div>
+      <ul>
+        {["home", "about", "contact", "work", "skills", "self-education"].map((item, index) => {
+          return (
+            <li key={index}>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
---
+++ frontend/src/constants/images.js
@@ -0,0 +1,7 @@
+import snecek_logo from "../assets/snecek_logo.jpg";
+
+const exports = {
+  snecek_logo,
+};
+
+export default exports;
