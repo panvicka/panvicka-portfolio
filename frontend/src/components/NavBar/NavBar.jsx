d76191d2496a32aeba8fc4adcf6480084effe40b
panvicka <panvicka@gmail.com>
2021-12-03 02:10:07 +0100

    navigation bar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
-import { LogoImg } from "./NavBar.styles";
+import { Navbar, LogoContainer, NavLinks, Menu, MenuContent } from "./NavBar.styles";
 import { images } from "../../constants";
 
 const NavBar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const menuArray = ["home", "about", "contact", "work", "skills", "self-education"];
+
   return (
-    <nav>
-      <div>
-        <LogoImg src={images.snecek_logo} alt="snail logo" className="rounded"></LogoImg>
-      </div>
-      <ul>
-        {["home", "about", "contact", "work", "skills", "self-education"].map((item, index) => {
+    <Navbar>
+      <LogoContainer>
+        <img src={images.snecek_logo} alt="snail logo" className="rounded" />
+      </LogoContainer>
+      <NavLinks>
+        {menuArray.map((item, index) => {
           return (
-            <li key={index}>
+            <li key={`link-${index}`}>
               <a href={`#${item}`}>{item}</a>
             </li>
           );
         })}
-      </ul>
-    </nav>
+      </NavLinks>
+
+      <Menu>
+        <HiMenu onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <MenuContent as={motion.div} whileInView={{ x: [300, 0] }} transition={{ duration: 1 }}>
+            <HiX onClick={() => setToggleMenu(false)} />
+            <NavLinks>
+              {menuArray.map((item, index) => {
+                return (
+                  <li key={`mobile-menu-${index}`}>
+                    <a href={`#${item}`} onClick={() => setToggleMenu(true)}>
+                      {item}
+                    </a>
+                  </li>
+                );
+              })}
+            </NavLinks>
+          </MenuContent>
+        )}
+      </Menu>
+    </Navbar>
   );
 };
 
--- frontend/src/components/NavBar/NavBar.styles.js
+++ frontend/src/components/NavBar/NavBar.styles.js
@@ -1,9 +1,144 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import * as globalStyles from "../../index.styles";
 
- 
-export const LogoImg = styled.img`
-  object-fit: cover;
+export const LogoContainer = styled(globalStyles.FlexCol)`
+  align-items: center;
+  justify-content: flex-start;
+
+  img {
+    object-fit: cover;
+    border-radius: 50%;
+    height: 130px;
+    width: 130px;
+
+    @media screen and (min-width: 1500px) {
+      height: 200px;
+      width: 200px;
+    }
+  }
+`;
+
+export const Menu = styled(globalStyles.FlexColCenter)`
+  width: 35px;
+  height: 35px;
   border-radius: 50%;
-  height: 100px;
-  width: 100px;
+  position: relative;
+  background: var(--secondary-color);
+  cursor: pointer;
+
+  svg {
+    width: 70%;
+    height: 70%;
+    color: var(--white-color);
+  }
+
+  @media screen and (min-width: 900px) {
+    display: none;
+  }
+`;
+
+export const MenuContent = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 10;
+  padding: 1rem;
+  width: 300px;
+  height: 100vh;
+  background-color: var(--white-color);
+  box-shadow: 0 0 20px pink;
+
+  svg {
+    width: 35px;
+    height: 35px;
+    color: var(--secondary-color);
+    margin: 0.5rem 1rem;
+  }
+
+  ul {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-direction: column;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+
+    li {
+      margin: 1rem;
+    }
+  }
+
+  @media screen and (min-width: 900px) {
+    display: none;
+  }
+`;
+
+export const NavLinks = styled(globalStyles.FlexRowCenter).attrs({ as: "ul" })`
+  flex: 1;
+  list-style: none;
+
+  li {
+    margin: 0 1rem;
+    cursor: pointer;
+
+    div {
+      width: 10px;
+      height: 10px;
+      background: transparent;
+      border-radius: 50%;
+      margin-bottom: 5px;
+    }
+
+    a {
+      color: var(--grey-color);
+      text-decoration: none;
+      flex-direction: column;
+      text-transform: uppercase;
+      font-weight: 400;
+      transition: all 0.2s ease-in-out;
+
+      &:hover {
+        color: var(--secondary-color);
+      }
+    }
+
+    &:hover {
+      div {
+        background: var(--secondary-color);
+      }
+    }
+  }
+
+  @media screen and (max-width: 900px) {
+    display: none;
+  }
+`;
+
+export const Navbar = styled(globalStyles.FlexRow).attrs({ as: "nav" })`
+  width: 100%;
+  border: 1px solid blue;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+  position: fixed;
+  z-index: 2;
+
+  // slightly transparent fallback
+  background: rgba(255, 255, 255, 0.8);
+
+  @supports (backdrop-filter: blur(4px)) {
+    // with backdrop-filter very transparent and blurred
+    background: rgba(255, 255, 255, 0.25);
+    backdrop-filter: blur(4px);
+    -webkit-backdrop-filter: blur(4px);
+  }
 `;
