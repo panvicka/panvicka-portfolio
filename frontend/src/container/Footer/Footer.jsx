f4ec0e484b06932baa84b4b1878d21d54789f805
panvicka <panvicka@gmail.com>
2022-03-06 17:08:10 +0100

    fixing link in the footer

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -22,7 +22,10 @@ function Footer() {
 
       <FooterSection>
         <div>
-          <FooterGitHubLink href="">
+          <FooterGitHubLink
+            href="https://github.com/panvicka/panvicka-portfolio"
+            aria-label="page Github"
+          >
             Check page <BsGithub />
           </FooterGitHubLink>
           <div>Created with React.JS and Sanity.io</div>
@@ -30,23 +33,43 @@ function Footer() {
         </div>
         <FooterSectionCredits>
           Credits:
-          <a href="https://www.flaticon.com/free-icons/circuit-board" title="circuit board">
+          <a
+            href="https://www.flaticon.com/free-icons/circuit-board"
+            title="circuit board"
+            aria-label="credits for icon circuit-board"
+          >
             {/* cspell:disable-next-line */}
             Circuit board icons created by Eucalyp
           </a>
-          <a href="https://www.flaticon.com/free-icons/microchip" title="microchip icons">
+          <a
+            href="https://www.flaticon.com/free-icons/microchip"
+            title="microchip icons"
+            aria-label="credits for icon microchip"
+          >
             {/* cspell:disable-next-line */}
             Microchip icons created by Freepik
           </a>
-          <a href="https://www.flaticon.com/free-icons/book" title="book icons">
+          <a
+            href="https://www.flaticon.com/free-icons/book"
+            title="book icons"
+            aria-label="credits for icon book"
+          >
             {/* cspell:disable-next-line */}
             Book icons created by itim2101
           </a>
-          <a href="https://www.flaticon.com/free-icons/layout" title="layout icons">
+          <a
+            href="https://www.flaticon.com/free-icons/layout"
+            title="layout icons"
+            aria-label="credits for icon layout"
+          >
             {/* cspell:disable-next-line */}
             Layout icons created by Freepik
           </a>
-          <a href="https://www.flaticon.com/free-icons/ui" title="ui icons">
+          <a
+            href="https://www.flaticon.com/free-icons/ui"
+            title="ui icons"
+            aria-label="credits for icon ui"
+          >
             {/* cspell:disable-next-line */}
             Ui icons created by surang
           </a>
